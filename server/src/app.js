0f021daba9d7cbabd50bb3c122462f9ae9dbc585
Jamshid <kalandarovjamshid01@gmail.com>
2023-05-11 14:47:05 +0500

    okay

--- server/src/app.js
+++ server/src/app.js
@@ -1,7 +1,9 @@
 const express = require("express");
 const app = express();
-const planets = require("./routes/planets");
+const cors = require("cors");
 
+const planets = require("./routes/planets");
+app.use(cors({ origin: "*" }));
 app.use(express.json());
 app.use("/planets", planets);
 module.exports = app;
